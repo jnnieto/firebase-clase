3dda306b8bb09bf15018c532a852b9b2ec33e934
Johann Nicolás Nieto Cárdenas <48180657+jnnieto@users.noreply.github.com>
2021-10-22 18:30:04 -0500

    Validacion de sesion de usuario

--- src/js/firebase.config.js
+++ src/js/firebase.config.js
@@ -1,5 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+import { 
+  getAuth,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,3 +22,42 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+window.salir = () => {
+  const auth = getAuth();
+  signOut(auth)
+    .then(() => {
+      document.location.href = "/src/views/index.html";
+    })
+    .catch((error) => {
+      alert("Se produce error al cerrar al sesión");
+      console.error(error);
+    })
+}
+
+export function verAutenticacion() {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log(user);
+      if (document.getElementById("login"))
+        document.getElementById("login").style.visibility = "hidden";
+
+      if (user.photoURL != null) {
+        document.getElementById("imgFotoUsuario").src = user.photoURL;
+      } else {
+        document.getElementById("imgFotoUsuario").src = "../../assets/img/user.jpg";
+      }
+
+      if (user.displayName) {
+        document.getElementById("labelUsuario").innerHTML = user.displayName;
+      }
+
+    } else if (!user) {
+      document.getElementById("navbarNav").style.visibility = "hidden";
+      document.getElementById("itemSalir").style.visibility = "hidden";
+      document.getElementById("imgFotoUsuario").style.visibility = "hidden";
+    }
+  });
+}
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,24 @@
-import { 
+import {
     getAuth,
     createUserWithEmailAndPassword,
     updateProfile,
     GoogleAuthProvider,
     signInWithPopup,
     GithubAuthProvider,
-    TwitterAuthProvider 
- } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
+    TwitterAuthProvider,
+    signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
+
+import {
+    verAutenticacion
+} from './firebase.config.js';
 
 const auth = getAuth();
+
+window.onload = () => {
+    verAutenticacion();
+}
+
 
 window.abrirMoldal = function abrirMoldal() {
     document.getElementById("alertaErrorRegistro").style.display = "none";
@@ -80,9 +90,9 @@ window.authGoogle = () => {
             // The signed-in user info.
             const user = result.user;
 
-            console.log(credential);
+            /* console.log(credential);
             console.log(token);
-            console.log(user);
+            console.log(user); */
             alert("Iniciado correctamente desde Google");
 
             // ...
@@ -91,7 +101,7 @@ window.authGoogle = () => {
             //const email = error.email;
             const errorMessage = error.message;
             document.getElementById("alertErrorLogueo").style.display = "block";
-            document.getElementById("alertErrorLogueo").innerHTML = errorMessage;     
+            document.getElementById("alertErrorLogueo").innerHTML = errorMessage;
             // The AuthCredential type that was used.
             //const credential = GoogleAuthProvider.credentialFromError(error);
             // ...
@@ -111,9 +121,9 @@ window.authGithub = () => {
             // The signed-in user info.
             const user = data.user;
 
-            console.log(credential);
+            /* console.log(credential);
             console.log(token);
-            console.log(user);
+            console.log(user); */
             alert("Iniciado correctamente desde Github");
         })
         .catch((error) => {
@@ -134,30 +144,59 @@ window.authTwitter = () => {
 
     signInWithPopup(auth, twitterProvider)
         .then((result) => {
-        // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
-        // You can use these server side with your app's credentials to access the Twitter API.
-        const credential = TwitterAuthProvider.credentialFromResult(result);
-        const token = credential.accessToken;
-        const secret = credential.secret;
+            // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+            // You can use these server side with your app's credentials to access the Twitter API.
+            const credential = TwitterAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const secret = credential.secret;
 
-        // The signed-in user info.
-        const user = result.user;
-        console.log(credential);
-        console.log(token);
-        console.log(user);
-        alert("Iniciado correctamente desde Github");
-        // ...
-    }).catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The AuthCredential type that was used.
-        const credential = TwitterAuthProvider.credentialFromError(error);
+            // The signed-in user info.
+            const user = result.user;
+            /* console.log(credential);
+            console.log(token);
+            console.log(user); */
+            alert("Iniciado correctamente desde Github");
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = TwitterAuthProvider.credentialFromError(error);
 
-        console.log(error);
-        // ...
-  });
+            console.log(error);
+            // ...
+        });
+
+}
+
+window.iniciarSesion = () => {
+
+    const correo = document.getElementById("correo").value;
+    const contrasena = document.getElementById("contrasena").value;
+
+    if (correo === "" || contrasena === "") {
+        document.getElementById("alertaErrorLogeo").style.display = "block";
+        document.getElementById("alertaErrorLogeo").innerHTML = "El correo y contraseña deben ser obligatorios";
+        return;
+    } else {
+        const autn = getAuth();
+        signInWithEmailAndPassword(auth, correo, contrasena)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                document.location.href = "/src/views/index.html";
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                document.getElementById("alertaErrorLogeo").style.display = "block";
+                document.getElementById("alertaErrorLogeo").innerHTML = errorMessage;
+            });
+
+    }
 
 }
